9d7e33c2b494da3be8abf67b8741bbf15ee96e95
Abhijeet_Prabhu <abhijeetprabhu15@gmail.com>
2015-06-22 12:23:56 +0530

    Setup Back-End Skeleton

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express'); //Node Module for express server
+
+var app=express();
+
+app.set('views',__dirname+'/server/views');
+console.log(__dirname+"   This is a variable");
+app.set('view engine','jade');
+app.use(express.static('public'));
+//Routes
+app.get('/',function(req,res)
+{
+
+res.render('index');
+
+});
+//req=>request res=>response
+app.listen(3020);
+console.log("Express Server is listening at port 3020");
